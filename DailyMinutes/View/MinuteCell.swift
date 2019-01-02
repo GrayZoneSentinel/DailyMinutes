e54102e8e06a6d4883367102b04c46f908b0cf02
Ernesto <ernesto@imac-de-ernesto.local>
2019-01-02 19:18:15 +0100

    Add like functionality implementation

--- DailyMinutes/View/MinuteCell.swift
+++ DailyMinutes/View/MinuteCell.swift
@@ -7,24 +7,36 @@
 //
 
 import UIKit
+import Firebase
 
 class MinuteCell: UITableViewCell {
     
-    // OUTLETS & ELEMENTS
+    // MARK: - OUTLETS & ELEMENTS
     @IBOutlet weak var userLbl: UILabel!
     @IBOutlet weak var timestampLbl: UILabel!
     @IBOutlet weak var minuteTxt: UILabel!
     @IBOutlet weak var starImageView: UIImageView!
     @IBOutlet weak var likesNumLbl: UILabel!
     
-
+    // MARK: - VARIABLES
+    private var minute: Minute!
+    
+    // MARK: - METHODS
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        // Implementation of the Likes functionality
+        let tap = UITapGestureRecognizer(target: self, action: #selector(likeTapped))
+        starImageView.addGestureRecognizer(tap)
+        starImageView.isUserInteractionEnabled = true
+        
     }
     
-    // FUNCTIONS
+    // MARK: - FUNCTIONS
     func configureCell(minute: Minute) {
+        //  In order to get the document Id that it is going to be updated due to the like incrementation function it is necesary to include the following code:
+            self.minute = minute
+        
         userLbl.text = minute.username
         minuteTxt.text = minute.minuteText
         likesNumLbl.text = String(minute.numLikes)
@@ -34,6 +46,11 @@ class MinuteCell: UITableViewCell {
         formatter.dateFormat = "d MMM Y, HH:mm"
         let timestamp = formatter.string(from: minute.timestamp)
         timestampLbl.text = timestamp
+    }
+    
+    @objc func likeTapped() {
+        // The function to update the DB register with an increment of one like for the relevant Minute
+        Firestore.firestore().collection(MINUTES_COL_REF).document(minute.documentId!).updateData([NUM_LIKES : minute.numLikes + 1])
     }
 
 }
